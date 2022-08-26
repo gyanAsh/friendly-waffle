6389fda512f32db91211e971dcf4c63ea1194d1b
gyanAsh <gyanash36@protonmail.com>
2022-08-26 22:41:25 +0530

    Css injection using extention

--- extention/background.js
+++ extention/background.js
@@ -1,8 +1,14 @@
-chrome.tabs.onActivated.addListener(tab => {
+chrome.tabs.onActivated.addListener((tab) => {
     chrome.tabs.get(
         tab.tabId,
         current_tab_info => {
             if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+
+                chrome.scripting.insertCSS({
+                    target: { tabId: tab.tabId },
+                    files:['./myStyle.css']
+                }, () => console.log("This is insert Css Script"));
+
                 chrome.scripting.executeScript({
                 target: {tabId:tab.tabId},
                 files: ['./foreground.js'],
--- extention/foreground.js
+++ extention/foreground.js
@@ -1,1 +1,6 @@
 console.log("foreground sup hai");
+document.querySelector('body').classList.add('spinspin');
+document.querySelector('.RNNXgb').classList.add('box-container');
+document.querySelector('.aajZCb').classList.add('suggest');
+document.querySelector('.sbct').classList.add('suggest-item');
+// document.querySelector('.FPdoLc').classList.add('ext-btn');
