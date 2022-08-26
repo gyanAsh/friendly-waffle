d0a6c954f3cccc01876266dd940c6791a3c9d005
gyanAsh <gyanash36@protonmail.com>
2022-08-26 17:19:19 +0530

    Extention files setup

---
+++ extention/background.js
@@ -0,0 +1,20 @@
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(
+        tab.tabId,
+        current_tab_info => {
+            if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+                chrome.scripting.executeScript({
+                target: {tabId:tab.tabId},
+                files: ['./foreground.js'],
+                }, () => console.log("It is alive!!"));
+            }
+            console.log(current_tab_info.url);
+        }
+    );
+    
+    
+    console.log(tab); 
+})
+
+
+console.log("It is the background file")
