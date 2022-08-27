d0e965c68773c33af58968e74d6d9482a95e7cd9
gyanAsh <gyanash36@protonmail.com>
2022-08-27 20:55:51 +0530

    Added and removed css

--- extention/foreground.js
+++ extention/foreground.js
@@ -3,4 +3,3 @@ document.querySelector('body').classList.add('spinspin');
 document.querySelector('.RNNXgb').classList.add('box-container');
 document.querySelector('.aajZCb').classList.add('suggest');
 document.querySelector('.sbct').classList.add('suggest-item');
-// document.querySelector('.FPdoLc').classList.add('ext-btn');
